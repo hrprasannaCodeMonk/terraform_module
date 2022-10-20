1087e0806644f8a34bca5c6417dd2c8602401a76
Prasanna H R <84318418+hrprasannaCodeMonk@users.noreply.github.com>
2022-10-20 08:51:13 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "dev4" {
+resource "aws_s3_bucket" "dev45" {
 
   bucket                = var.bucket
   #bucket_prefix         = var.bucket_prefix
   force_destroy         = var.force_destroy
 
   tags = {
-    Name        = "dev4"
+    Name        = "dev45"
   }
 }
